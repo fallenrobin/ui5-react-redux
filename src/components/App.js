9a381401cc5283fc476a44da5900bd5aea94a89f
Juliette Lelchuk <juliettelelchuk@mindsetconsulting.com>
2022-07-28 10:36:47 -0500

    delete irrelevent boilerplate files, create components folder

--- src/components/App.js
+++ src/components/App.js
@@ -9,9 +9,7 @@ import {
   Avatar
 } from '@ui5/webcomponents-react';
 
-
 import HomePage from './HomePage';
-import './App.css';
 
 function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { ThemeProvider } from '@ui5/webcomponents-react';
 import { store } from './Redux/store';
-import App from './App';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 

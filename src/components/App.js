21fc0809b283b62637acd513b8536904bae8f66b
Juliette Lelchuk <juliettelelchuk@mindsetconsulting.com>
2022-08-02 10:40:39 -0500

    add react-router

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,5 @@
+import { HashRouter as Router, Route } from "react-router-dom";
+
 import {
   FlexBox,
   FlexBoxAlignItems,
@@ -17,13 +19,13 @@ function App() {
       <ShellBar
         // logo={<MDXCreateElement alt="SAP Logo" mdxType="img" originalType="img" src="https://sap.github.io/ui5-webcomponents/assets/images/sap-logo-svg.svg"/>}
         onLogoClick={function noRefCheck() { }}
-        onMenuItemClick={function noRefCheck() { }}
-        onProfileClick={function noRefCheck() { }}
         primaryTitle="Mindset Skill Tracker"
         profile={<Avatar></Avatar>}
         secondaryTitle=""
       />
+      <Router>
         <HomePage />
+      </Router>
     </>
   );
 }

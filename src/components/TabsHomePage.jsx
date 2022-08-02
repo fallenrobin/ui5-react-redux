41ca2ed5889fff0e9aa65cce21030cbc1b27c72e
Juliette Lelchuk <juliettelelchuk@mindsetconsulting.com>
2022-08-02 15:20:06 -0500

    edit TabsHomePage

--- src/components/TabsHomePage.jsx
+++ src/components/TabsHomePage.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import {
     TabContainer,
     Tab,
     TabSeparator
 } from '@ui5/webcomponents-react';
+
+import HomePage from './HomePage';
 
 import "@ui5/webcomponents-icons/dist/radar-chart";
 import "@ui5/webcomponents-icons/dist/work-history";
@@ -12,31 +15,55 @@ import "@ui5/webcomponents-icons/dist/activity-individual";
 
 
 function TabsHomePage() {
+
+    const [selectedTab, setSelectedTab] = useState('');
+    const dispatch = useDispatch();
+
+    const handleMySkills = () => {
+        console.log('click');
+        // dispatch({type: 'HOME'})
+    }
+
+    const handleChart = () => {
+        console.log('click chart');
+        // dispatch({type: 'CHART'})
+    }
+    const handleProjects = () => {
+        console.log('click projects');
+        // dispatch({type: 'PROJECTS'})
+    }
+
+
     return (
         <TabContainer
-            className=""
-            onTabSelect={function noRefCheck() { }}
+            onTabSelect={handleMySkills}
         >
+
             <Tab
                 additionalText="5"
                 icon="activity-individual"
-                selected
                 text="My Skills"
             >
                 My Skills
             </Tab>
+
             <TabSeparator />
+
             <Tab
                 additionalText="20"
                 icon="radar-chart"
                 text="Team Skills"
+                selected
+                // onTabSelect={handleChart}
             >
                 Team Skills
             </Tab>
+
             <Tab
                 additionalText="20"
                 icon="work-history"
                 text="My Projects"
+                // onTabSelect={handleProjects}
             >
                 My Projects
             </Tab>

d19e3ef5969b8a294dec824650c676217d28198b
Juliette Lelchuk <juliettelelchuk@mindsetconsulting.com>
2022-08-02 14:09:04 -0500

    add Panel to Projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 
+import {
+  Panel,
+  Title,
+  Text
+} from '@ui5/webcomponents-react';
+
+
 function Projects() {
   return (
-    <div>Projects</div>
+    <Panel
+      headerText="Novacure"
+      onToggle={function noRefCheck() { }}
+    >
+      <Title level="H3">
+        Project dates: 01/01/2020 - 02/01/2021
+      </Title>
+      <Text>
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+      </Text>
+    </Panel>
   )
 }
 

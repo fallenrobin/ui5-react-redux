d60539475198a4cf9bb2f43915a894448a381ddf
Juliette Lelchuk <juliettelelchuk@mindsetconsulting.com>
2022-08-03 13:21:13 -0500

    create ProjectItem, map through projectReducer

---
+++ src/components/ProjectItem.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+import {
+    FlexBox,
+    Panel,
+    Title,
+    Text
+} from '@ui5/webcomponents-react';
+
+function ProjectItem() {
+    return (
+        <>
+            <Panel
+                headerText="Novacure"
+                onToggle={function noRefCheck() { }}
+                style={{ width: '50%' }}
+            >
+                <Title level="H4">
+                    Project dates: 01/01/2020 - 02/01/2021
+                </Title>
+                <Text>
+                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                </Text>
+            </Panel>
+            <Panel
+                headerText="Novacure"
+                onToggle={function noRefCheck() { }}
+                style={{ width: '50%' }}
+            >
+                <Title level="H4">
+                    Project dates: 01/01/2020 - 02/01/2021
+                </Title>
+                <Text>
+                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                </Text>
+            </Panel>
+        </>
+    )
+}
+
+export default ProjectItem
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,47 +1,29 @@
 import React from 'react'
 import projectReducer from '../Redux/projectReducer';
+import ProjectItem from './ProjectItem';
 
 import {
   FlexBox,
-  Panel,
-  Title,
-  Text
 } from '@ui5/webcomponents-react';
 import { useSelector } from 'react-redux';
 
 
 function Projects() {
 
-const projectData = (useSelector(store => store.projectReducer));
+  const projectData = (useSelector(store => store.projectReducer));
 
-console.log(projectData);
+  console.log(projectData);
 
   return (
     <FlexBox>
-      <Panel
-        headerText="Novacure"
-        onToggle={function noRefCheck() { }}
-        style={{ width: '50%' }}
-      >
-        <Title level="H4">
-          Project dates: 01/01/2020 - 02/01/2021
-        </Title>
-        <Text>
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-        </Text>
-      </Panel>
-      <Panel
-        headerText="Novacure"
-        onToggle={function noRefCheck() { }}
-        style={{ width: '50%' }}
-      >
-        <Title level="H4">
-          Project dates: 01/01/2020 - 02/01/2021
-        </Title>
-        <Text>
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-        </Text>
-      </Panel>
+      {projectData.map((project) => {
+        return (
+          <ProjectItem
+            project={project}
+          />
+        )
+      })}
+
     </FlexBox>
   )
 }

ca78f099527127c8cbd93c71400bd4fcc0c3b23d
Juliette Lelchuk <juliettelelchuk@mindsetconsulting.com>
2022-08-03 13:02:51 -0500

    create projectReducer

---
+++ src/Redux/projectReducer.js
@@ -0,0 +1,17 @@
+const initialState = [
+
+]
+
+ function projectReducer (state = initialState, action) => {
+  switch (type) {
+
+  case first:
+    return { ...state, ...payload }
+
+  default:
+    return state
+  }
+}
+
+
+export default projectReducer;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import {
+  FlexBox,
   Panel,
   Title,
   Text
@@ -9,17 +10,32 @@ import {
 
 function Projects() {
   return (
+    <FlexBox>
       <Panel
         headerText="Novacure"
         onToggle={function noRefCheck() { }}
+        style={{ width: '50%' }}
       >
-        <Title level="H3">
+        <Title level="H4">
           Project dates: 01/01/2020 - 02/01/2021
         </Title>
         <Text>
           Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
         </Text>
       </Panel>
+      <Panel
+        headerText="Novacure"
+        onToggle={function noRefCheck() { }}
+        style={{ width: '50%' }}
+      >
+        <Title level="H4">
+          Project dates: 01/01/2020 - 02/01/2021
+        </Title>
+        <Text>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+        </Text>
+      </Panel>
+    </FlexBox>
   )
 }
 
